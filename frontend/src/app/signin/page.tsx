2bd10be1185561209cffb48a6621d1d0fdb01b8f
yosri-boujnah <yosriboujnah84@gmail.com>
2024-07-10 14:24:45 +0100

    'loginLastvir'

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -1,58 +1,124 @@
+'use client'
 import Image from "next/image"
 import signinPhoto from "../../image/make-a-logo-online.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faAt} from '@fortawesome/free-solid-svg-icons'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
+import Link from "next/link"
+import axios from "axios"
+import { NextApiRequest, NextApiResponse } from 'next'
+import { useState } from "react";
+
 export default function signin(){
- 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [Error, setError] = useState('');
+  const [role,setRole]=useState('')
+
+  async function handleSubmit() {
+    // event.preventDefault();
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/api/authentication/login', {
+        email,
+        password
+      });
+      console.log(response.data);
+      const { token, role} = response.data;
+      localStorage.setItem('token', token);
+      
+      if (role === 'instructor') {
+          //yet3da lel dachbord mta3 instructor
+          return setRole('instructor')
+      } else if (role === 'student') {
+       //yet3ada lel dachbord mta3 student 
+       //bel <link> mta3 sign up button 
+       return setRole('student')
+
+      } else {
+        setError('Invalid role. Please contact support.')
+        setError('Invalid role. Please contact support.');
+
+      }
+      if (response.status === 200) {
+        console.log("Login successful");
+      } else {
+      console.log('error');
+
+        console.error("Authentication failed");
+      }
+
+    } catch (error) {
+      setError(error.response.data.msg);
+    }
+  }
+console.log(Error);
+
     return (
-     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="flex bg-white shadow-lg rounded-lg overflow-hidden">
-        <div className="w-1/2 p-8">
-          <h2 className="text-2xl font-bold text-gray-800 mb-2">
+     <div className="flex items-center justify-center mt-14">
+      <div className=" login-page flex gap-4  overflow-hidden">
+      <div className="">
+            <Image src={signinPhoto}
+            width={800}
+            alt="Picture of the author"
+            className=""
+          />
+        </div>
+        <div className="">
+          <h2 className="text-4xl font-bold text-gray-800 mb-2">
             Ready to start your success story?
           </h2>
-          <p className="text-gray-600 mb-4">
+          <p className="text-gray-600 font-semibold mb-4">
           Please enter your email address and password to proceed
           </p>
           <form className="space-y-4">
+          {Error&&<span className="error-message mt-8 flex items-center justify-center bg-[#EF665B] p-2 w-68 text-sm font-bold text-[#fff] rounded-lg" id="name-error"> {Error}</span>}
             <div>
-              <label htmlFor="Email" className="block text-sm font-medium text-gray-700">
+              <label htmlFor="Email" className="block font-semibold text-gray-700">
                 Email
               </label>
+              <div className="flex border mt-4 p-2.5 rounded-lg">
+              <FontAwesomeIcon icon={faAt} className="w-4 ml-1.5 text-[#171a29]" />
               <input
                 id="Email"
-                className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#a200ff] text-black"
-                placeholder="Email"
+                className="w-full ml-2 outline-none"
+                placeholder="Enter your email address"
                 type="text"
+                onChange={(e) => setEmail(e.target.value)}
                 required
               />
+              </div>
+              
             </div>
             <div>
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                password
+              <label htmlFor="password" className="block font-semibold  text-gray-700">
+                Password
               </label>
+              <div className="flex border mt-4 p-2.5 rounded-lg">
+              <FontAwesomeIcon icon={faLock} className="w-4 ml-1.5 text-[#171a29]" />
+
               <input
                 id="password"
-                className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#a200ff] text-black"
-                placeholder="password"
+                className="w-full ml-2 outline-none"
+                placeholder="Enter Password"
                 type="password"
+                onChange={(e) => setPassword(e.target.value)}
                 required
               />
+              </div>
             </div>
-            <div className="mt-6">
-              <button
-                className="w-full px-6 py-3 bg-[#a200ff] text-white rounded-lg hover:bg-white hover:text-[#a200ff] border-2 border-[#a200ff] transition-colors duration-300"
-                type="submit"
-              >
-                Sign up
-              </button>
+            <div className="">
             </div>
           </form>
+              <button
+                className="w-full px-6 py-3 mt-3  bg-[#a200ff] text-white rounded-lg hover:bg-white hover:text-[#a200ff] border-2 border-[#a200ff] transition-colors duration-300"
+                
+                onClick={()=>{handleSubmit()}}
+              >
+                Login
+              </button>
+          <div className="signup-sugg">
+          <h5 className="mt-3 flex items-center justify-center">Don't have an account? <Link href='/signup' className="text-[#a200ff] ml-3 font-bold">Sign up</Link> </h5>
         </div>
-        <div className="w-1/1 relative m-5">
-            <Image src={signinPhoto}
-            width={400}
-            height={400}
-            alt="Picture of the author"
-          />
         </div>
       </div>
     </div>
